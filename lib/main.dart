6f58c3eff58ce07e852e81b2f43f8149d22d8898
DXie123 <1541278149@qq.com>
2019-09-12 13:22:31 +0800

    rewrite keyboard layout

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,33 @@ class _HomePageState extends State<HomePage> {
   String latexExpression = '';
   String mathExpression = '';
   String resultText = '';
+  Map keyboard = {
+    '1' : Text('1'),
+    '3' : Text('3'),
+    '7' : Text('7'),
+    '0' : Text('0'),
+    '+' : Text('+'),
+    '-' : Text('-'),
+    '/' : Text('÷'),
+    '^' : Text('^'),
+    '.' : Text('.'),
+    '(' : Text('('),
+    ')' : Text(')'),
+    'e' : Text('e'),
+    '\\pi' : Text('pi'),
+    '\\\\times' : Text('×'),
+    '\\sqrt' : Text('sqrt'),
+    '\\sin' : Text('sin'),
+    '\\arcsin' : Text('asin'),
+    '\\log' : Text('log'),
+    '\\|' : Text('abs'),
+    'x' : Text('x'),
+    'Left' : Icon(Icons.arrow_back),
+    'Right' : Icon(Icons.arrow_forward),
+    'AC' : Icon(Icons.delete),
+    'BackSpace' : Icon(Icons.backspace),
+    '=' : Text('='),
+  };
 
   @override
   void initState() {
@@ -100,185 +127,70 @@ class _HomePageState extends State<HomePage> {
           Text('Math: ' + mathExpression),
           Text('Output: ' + resultText),
           Expanded(
-            child: GridView.count(
-              crossAxisCount: 5,
-              children: <Widget>[
-                MyButton(
-                  onPressed: () {
-                    addExpression('/');
-                  },
-                  text: 'frac',
-                ),
-                MyButton(
-                  onPressed: () {
-                    delExpression();
-                  },
-                  text: 'Del',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('0');
-                  },
-                  text: '0',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('1');
-                  },
-                  text: '1',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('2');
-                  },
-                  text: '2',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('4');
-                  },
-                  text: '4',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('7');
-                  },
-                  text: '7',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('.');
-                  },
-                  text: '.',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('^');
-                  },
-                  text: '^',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('e');
-                  },
-                  text: 'e',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('\\\\times');
-                  },
-                  text: '*',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('+');
-                  },
-                  text: '+',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('-');
-                  },
-                  text: '-',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('\\sqrt');
-                  },
-                  text: 'sqrt',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('\\sin');
-                    addExpression('\\(');
-                  },
-                  text: 'sin',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('\\(');
-                  },
-                  text: '(',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('\\)');
-                  },
-                  text: ')',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('\\log');
-                    addExpression('_');
-                  },
-                  text: 'log',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('\\arcsin');
-                    addExpression('\\(');
-                  },
-                  text: 'asin',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('\\|');
-                  },
-                  text: 'abs',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('\\int');
-                  },
-                  text: 'int',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addExpression('x');
-                  },
-                  text: 'x',
-                ),
-                MyButton(
-                  onPressed: () {
-                    addKey('Right');
-                  },
-                  text: 'Right',
-                ),
-                MyButton(
-                  onPressed: () {
-                    delAllExpression();
-                    setState(() {
-                      mathExpression = '';
-                      resultText = '';
-                    });
-                  },
-                  text: 'AC',
-                ),
-                MyButton(
-                  onPressed: () {
-                    setState(() {
-                      LatexParser lp = LatexParser();
-                      lp.parse(latexExpression);
-                      mathExpression = lp.result.toString();
-                      if (lp.result.isSuccess) {
-                        print(lp.result.value is String);
-                      }
-                      if (mathExpression.contains('x')) {
-                        MyFunction f = MyFunction(lp.result.value);
-                        resultText = f.calc(1).toString();
-                      } else {
-                        Parser p = new Parser();
-                        Expression exp = p.parse(lp.result.value);
-                        ContextModel cm = new ContextModel();
-                        double eval = exp.evaluate(EvaluationType.REAL, cm);
-                        resultText = intCheck(eval).toString();
-                        print(eval);
-                      }
-                    });
-                  },
-                  text: '=',
-                ),
-              ],
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 5,
+              ),
+              itemCount: keyboard.length,
+              itemBuilder: (context, index) => MyButton(
+                onPressed: () {
+                  var cmd = keyboard.keys.elementAt(index);
+                  switch (cmd) {
+                    case '\\sin':
+                      continue trigonometric;
+                    trigonometric:
+                    case '\\arcsin':
+                      addExpression(cmd);
+                      addExpression('(');
+                      break;
+                    case '\\log':
+                      addExpression(cmd);
+                      addExpression('_');
+                      break;
+                    case 'Right':
+                      continue movecursor;
+                    movecursor:
+                    case 'Left':
+                      addKey(cmd);
+                      break;
+                    case 'AC':
+                      delAllExpression();
+                      setState(() {
+                        mathExpression = '';
+                        resultText = '';
+                      });
+                      break;
+                    case 'BackSpace':
+                      delExpression();
+                      break;
+                    case '=':
+                      setState(() {
+                        LatexParser lp = LatexParser();
+                        lp.parse(latexExpression);
+                        mathExpression = lp.result.toString();
+                        if (lp.result.isSuccess) {
+                          print(lp.result.value is String);
+                        }
+                        if (mathExpression.contains('x')) {
+                          MyFunction f = MyFunction(lp.result.value);
+                          resultText = f.calc(1).toString();
+                        } else {
+                          Parser p = new Parser();
+                          Expression exp = p.parse(lp.result.value);
+                          ContextModel cm = new ContextModel();
+                          double eval = exp.evaluate(EvaluationType.REAL, cm);
+                          resultText = intCheck(eval).toString();
+                          print(eval);
+                        }
+                      });
+                      break;
+                    default: 
+                      addExpression(cmd);
+                      break;
+                  }
+                },
+                child: keyboard.values.elementAt(index),
+              ),
             ),
           ),
         ],
--- lib/mybutton.dart
+++ lib/mybutton.dart
@@ -1,38 +1,37 @@
 import 'package:flutter/material.dart';
 
-class MyButton extends StatelessWidget{
-  
-  final String text;
+class MyButton extends StatelessWidget {
+  final Widget child;
   final VoidCallback onPressed;
   final double _width = 40.0; //40.0 is better
-  
+
   const MyButton({
-    @required this.text,
+    @required this.child,
     @required this.onPressed,
   });
 
   @override
-  Widget build (BuildContext context){
-    return Container(
-      height: _width * 2,
-      width: _width * 2,
-      alignment: Alignment.center,
-      child: InkResponse(
-        radius: _width * 1.2,
-        splashFactory: InkRipple.splashFactory,
-        highlightColor: Colors.transparent,
-        onTap: onPressed,
-        child: Container(
-          height: _width * 2,
-          width: _width * 2,
-          alignment: Alignment.center,
-          child: Text(
-            text,
-            style: TextStyle(
-              fontSize: _width / 1.3,
-              color: Colors.black,
-              fontFamily: 'RobotoMono',
-            ),
+  Widget build(BuildContext context) {
+    return DefaultTextStyle(
+      style: TextStyle(
+        fontSize: _width / 1.3,
+        color: Colors.black,
+        fontFamily: 'RobotoMono',
+      ),
+      child: Container(
+        height: _width * 2,
+        width: _width * 2,
+        alignment: Alignment.center,
+        child: InkResponse(
+          radius: _width * 1.2,
+          splashFactory: InkRipple.splashFactory,
+          highlightColor: Colors.transparent,
+          onTap: onPressed,
+          child: Container(
+            height: _width * 2,
+            width: _width * 2,
+            alignment: Alignment.center,
+            child: child,
           ),
         ),
       ),
