bbcccd3a2799135e51eb8e9f2d92e580f6485391
DXie123 <1541278149@qq.com>
2019-10-30 20:59:26 +0800

    upgrade flutter sdk

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\dylan\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Code\Android\calculator"
+export "FLUTTER_APPLICATION_PATH=D:\Code\Android\num_plus_plus"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
 export "FLUTTER_FRAMEWORK_DIR=C:\Users\dylan\flutter\bin\cache\artifacts\engine\ios"
-export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NAME=1.3.0"
 export "FLUTTER_BUILD_NUMBER=1"
